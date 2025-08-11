f6b300ad022fc22ac34ecd0683128914c8e418e5
Arge2004 <argenismedina0424@gmail.com>
2025-08-11 12:18:47 -0500

    [FEAT] Added funtionality to measure analytics by google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "@/layouts/Header";
 import "./globals.css";
+import AnalyticsListener from "./analitycs-listener";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +30,27 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-sky-200 to-sky-300 h-full min-h-screen `}
       >
+        {/* Google Analytics */}
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
+          </>
+        )}
         <Header />
+        <AnalyticsListener />
         <main className="max-w-8xl">
           {children}
         </main>
